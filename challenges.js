0d2f795b0d07f4347ce33dee08c015f4ac4f7eb2
sivaShilpa <shilpa.vchand@gmail.com>
2023-03-02 16:53:39 -0500

    complete Challenge: 09-charCount

--- challenges.js
+++ challenges.js
@@ -281,7 +281,16 @@ charCount('hello') //=> { h: 1, e: 1, l: 2, o: 1 }
 charCount('Today is fantastic!') //=> { T: 1, o: 1, d: 1, a: 3, y: 1, ' ': 2, i: 2, s: 2, f: 1, n: 1, t: 2, c: 1, '!': 1 }
 -----------------------------------------------------------------*/
 // Your solution for 09-charCount here:
-
+function charCount(str){
+  let strArray = str.split("")
+  let returnObject = strArray.reduce((acc,strArr) => {
+    acc[strArr] = acc[strArr]? acc[strArr]+1 : 1
+    return acc
+  }, {})
+  return returnObject
+}
+console.log(charCount('hello'))
+console.log(charCount('Today is fantastic!'))
 
 
 
