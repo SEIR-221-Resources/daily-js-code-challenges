ce27411d6fb9f22c1097e1c91b92023501402b3d
tpett20 <thomaspettersen20@gmail.com>
2023-03-09 18:32:41 -0500

    Challenge 12 complete.

--- challenges.js
+++ challenges.js
@@ -348,8 +348,13 @@ hammingDistance('abc', 'ab'); //=> NaN
 -----------------------------------------------------------------*/
 // Your solution for 12-hammingDistance here:
 
-
-
+function hammingDistance(str1, str2) {
+  let hamDist = 0
+  if (str1.length !== str2.length) return NaN
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) hamDist++
+  } return hamDist
+}
 
 
 /*-----------------------------------------------------------------
