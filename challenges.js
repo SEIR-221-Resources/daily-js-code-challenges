94173566cc0c7588c5fffc05585a746c712caa5a
mgballou <matthewbuiltthat@gmail.com>
2023-03-16 19:28:31 -0400

    Solve hammingDistance

--- challenges.js
+++ challenges.js
@@ -385,8 +385,21 @@ hammingDistance('abc', 'ab'); //=> NaN
 -----------------------------------------------------------------*/
 // Your solution for 12-hammingDistance here:
 
+function hammingDistance (str1, str2) {
+  if (str1.length !== str2.length) {
+    return NaN
+  } else {
+    let count = 0
+    for (let i=0; i < str1.length; i++) {
+      if (str1[i] !== str2[i]){
+        count ++
+      }
+    }
+    return count
+  }
+}
 
-
+console.log(hammingDistance('abc', 'a2c'))
 
 
 /*-----------------------------------------------------------------
