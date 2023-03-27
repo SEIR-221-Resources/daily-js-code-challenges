11b7d22f3691c102b17a6bb227dd2946f075cfde
sivaShilpa <shilpa.vchand@gmail.com>
2023-03-27 17:00:22 -0400

    complete Challenge: 20-isPrime

--- challenges.js
+++ challenges.js
@@ -761,9 +761,23 @@ isPrime(29) //=> true
 isPrime(200) //=> false
 -----------------------------------------------------------------*/
 // Your solution for 20-isPrime here:
+function isPrime(num){
+  
+  if(num === 1) return false
+  else if(num === 2) return true
+  else{
+    for(let i=2; i<num; i++){
+      if(num % i === 0) return false
+    }
+    return true
+  }
+}
 
-
-
+console.log(isPrime(2))
+console.log(isPrime(3))
+console.log(isPrime(4))
+console.log(isPrime(29))
+console.log(isPrime(200))
 
 
 /*-----------------------------------------------------------------
